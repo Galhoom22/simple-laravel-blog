6919a95c148cf8eed22e7291e997700425ee3095
galhoom <galhoom@Galhoom>
2025-09-29 04:06:01 +0300

    feat: Add seeder and factory for posts

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(4), // Generate a fake sentence with 4 words
+            'body' => fake()->paragraph(10) // Generate a fake paragraph with 10 sentences
+        ];
+    }
+}
